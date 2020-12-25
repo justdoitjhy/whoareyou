8ea054407b64278c677c22969f198c55d14c749b
justdoitjhy <justdoitjhy@gmail.com>
2020-12-26 03:29:56 +0900

    first test server start code

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hi');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+function handleListening(){
+    console.log(`Listening on: http://localhost:${port}`);
+}
+
+function handleHome(req, res){
+    res.send("Hello, Homepage");
+}
+
+function handleProfile(req, res){
+    res.send("who are you");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(port, handleListening);
